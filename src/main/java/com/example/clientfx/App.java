1007d1e4c97327437dd8ebc3f771bb86251ab629
JustOverFR <jeanjuste.lefebvre@gmail.com>
2023-04-12 16:12:29 +0200

    ok

--- src/main/java/com/example/clientfx/App.java
+++ src/main/java/com/example/clientfx/App.java
@@ -21,104 +21,204 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+/**
+ * Classe App qui sert de point d'entrée pour l'application du client
+ */
 public class App extends Application {
-    private static final String ipServer = "localhost";
-    private static final int portServer = 1234;
-
     private Socket socket;
     private PrintWriter printWriter;
+
     private StringProperty messages = new SimpleStringProperty("");
 
+    /**
+     * Fonction principale pour lancer l'application
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
+    /**
+     * Fonction qui sert à lancer l'application
+     *
+     * @param primaryStage Stage principal de l'application
+     *
+     * {@inheritDoc}
+     */
     public void start(Stage primaryStage) {
+        connectionPage(primaryStage);
+    }
+
+    /**
+     * Fonction qui sert à afficher la page de connexion
+     *
+     * @param stage Stage principal de l'application
+     */
+    private void connectionPage(Stage stage) {
+        // Crée le conteneur principal de la page de connexion
         VBox root = new VBox(10);
         root.setAlignment(Pos.CENTER);
 
-        Label usernameLabel = new Label("Veuillez entrer votre pseudo :");
+        // Crée les composants de la page de connexion
+        Label ipLabel = new Label("IP Address:");
+        TextField ipTextField = new TextField();
+        Label portLabel = new Label("Port:");
+        TextField portTextField = new TextField();
+        Label usernameLabel = new Label("Username:");
         TextField usernameTextField = new TextField();
-        Button connectButton = new Button("Se connecter");
-        connectButton.disableProperty().bind(Bindings.isEmpty(usernameTextField.textProperty()));
+        Button connectButton = new Button("Connect");
 
+        // Désactive le bouton de connexion si un des champs est vide
+        connectButton.disableProperty().bind(
+                Bindings.isEmpty(ipTextField.textProperty())
+                        .or(Bindings.isEmpty(portTextField.textProperty()))
+                        .or(Bindings.isEmpty(usernameTextField.textProperty()))
+        );
+
+        // Lorsque le bouton de connexion est cliqué, on se connecte au serveur
+        connectButton.setOnAction(event -> {
+            // On récupère les valeurs des champs
+            String ipAddress = ipTextField.getText().trim();
+            int port = Integer.parseInt(portTextField.getText().trim());
+            String username = usernameTextField.getText().trim();
+
+            // On vérifie que les champs ne sont pas vides
+            if (!ipAddress.isEmpty() && !username.isEmpty()) {
+                // On se connecte au serveur et on affiche la page de chat
+                connectToServer(ipAddress, port, username);
+                chatPage(stage);
+            }
+        });
+
+        // On ajoute les composants au conteneur principal
+        root.getChildren().addAll(ipLabel, ipTextField, portLabel, portTextField, usernameLabel, usernameTextField, connectButton);
+
+        // On affiche la page de connexion
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("Chat Client - Connection");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Fonction qui sert à afficher la page de chat
+     *
+     * @param stage Stage principal de l'application
+     */
+    private void chatPage(Stage stage) {
+        // Crée le conteneur principal de la page de chat
+        VBox root = new VBox(10);
+        root.setAlignment(Pos.CENTER);
+
+        // La zone de texte qui affiche les messages
         TextArea chatArea = new TextArea();
         chatArea.textProperty().bind(messages);
         chatArea.setEditable(false);
 
+        // La zone de texte pour écrire un message et le bouton d'envoi
         TextField messageTextField = new TextField();
-        Button sendButton = new Button("Envoyer");
+        Button sendButton = new Button("Send");
         sendButton.disableProperty().bind(Bindings.isEmpty(messageTextField.textProperty()));
 
-        connectButton.setOnAction(event -> {
-            String username = usernameTextField.getText().trim();
-            if (!username.isEmpty()) {
-                connectToServer(username);
-                usernameTextField.clear();
-            }
-        });
-
+        // Lorsque le bouton d'envoi est cliqué, on envoie le message au serveur
         sendButton.setOnAction(event -> {
+            // On récupère le message
             String message = messageTextField.getText().trim();
+
+            // On vérifie que le message n'est pas vide et que la connexion est ouverte
             if (!message.isEmpty() && socket != null && !socket.isClosed()) {
+                // On envoie le message au serveur
                 printWriter.println(message);
                 printWriter.flush();
                 messageTextField.clear();
             }
         });
 
+        // Le bouton de déconnexion
+        Button disconnectButton = new Button("Disconnect");
+        disconnectButton.setOnAction(event -> {
+            // On ferme la connexion et on affiche la page de connexion
+            closeResources();
+            connectionPage(stage);
+        });
+
+        // Un Hbox est un conteneur horizontal qui permet d'aligner les composants horizontalement
         HBox messageBox = new HBox(10, messageTextField, sendButton);
         messageBox.setAlignment(Pos.CENTER);
 
-        root.getChildren().addAll(usernameLabel, usernameTextField, connectButton, chatArea, messageBox);
+        // On ajoute les composants au conteneur principal
+        root.getChildren().addAll(chatArea, messageBox, disconnectButton);
 
-        primaryStage.setOnCloseRequest(event -> {
+        // Lorsque la fenêtre est fermée, on ferme la connexion
+        stage.setOnCloseRequest(event -> {
             closeResources();
             Platform.exit();
             System.exit(0);
         });
 
+        // On affiche la page de chat
         Scene scene = new Scene(root, 600, 400);
-        primaryStage.setTitle("Chat Client");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setTitle("Chat Client - Chat");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    private void connectToServer(String username) {
+    /**
+     * Fonction qui sert à se connecter au serveur
+     *
+     * @param ipAddress Adresse IP du serveur
+     * @param port      Port du serveur
+     * @param username  Nom d'utilisateur
+     */
+    private void connectToServer(String ipAddress, int port, String username) {
         try {
-            socket = new Socket(ipServer, portServer);
-            System.out.println("Connexion établie avec le serveur");
+            socket = new Socket(ipAddress, port);
+            System.out.println("Connecté au serveur");
 
+            // Point d'entrée pour envoyer des messages au serveur
             printWriter = new PrintWriter(socket.getOutputStream());
 
+            // On envoie le nom d'utilisateur au serveur
             printWriter.println(username);
 
-            // Créer un objet Runnable pour lire les messages du serveur
+            // On crée un thread qui lit les messages du serveur
             Runnable readMessages = () -> {
                 try {
+                    // Point d'entrée pour lire les messages du serveur
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                    // On lit les messages du serveur
                     String message;
                     while ((message = bufferedReader.readLine()) != null) {
                         messages.set(messages.get() + "\n" + message);
                     }
                 } catch (IOException e) {
+                    // Affiche la pile d'appels de l'exception
                     e.printStackTrace();
                 }
             };
 
-            // Démarrer un thread pour lire les messages du serveur
+            // On démarre le thread
             new Thread(readMessages).start();
         } catch (IOException e) {
-            System.out.println("Impossible de se connecter au serveur");
+            System.out.println("Erreur lors de la connexion au serveur");
+            // Affiche la pile d'appels de l'exception
             e.printStackTrace();
         }
     }
 
+    /**
+     * Fonction qui sert à fermer les points d'entrée et la connexion.
+     * Cette fonction est appelée lorsque la fenêtre est fermée.
+     */
     private void closeResources() {
+        // Vérifie que les points d'entrée ne sont pas null
         if (printWriter != null) {
             printWriter.close();
         }
+        // Vérifie que la connexion n'est pas null
         if (socket != null) {
             try {
                 socket.close();
